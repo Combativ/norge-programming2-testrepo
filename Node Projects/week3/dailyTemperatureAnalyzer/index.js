8c728422969e3631a9f2aa6ecac801e81810aa9d
jzurmueh <jzurmueh@hs-mittweida.de>
2026-01-15 18:48:15 +0100

    Update index.js, figlet, .package-lock.json, and 987 more files

--- Node Projects/week3/dailyTemperatureAnalyzer/index.js
+++ Node Projects/week3/dailyTemperatureAnalyzer/index.js
@@ -3,24 +3,32 @@
 import { program } from "commander";
 import chalk from "chalk";
 import inquirer from "inquirer";
+import ora from "ora";
+import figlet from "figlet";
 
-program
-  .version("1.0.0")
-  .description("My Node CLI");
+program.version("1.0.0").description("My Node CLI");
 
-program
-  .action(() => {
-    inquirer
-      .prompt([
-        {
-          type: "input",
-          name: "name",
-          message: "What is your name?",
-        },
-      ])
-      .then((answers) => {
-        console.log(chalk.green(`Hey there, ${answers.name}!`));
-      });
-  });
+program.action(() => {
+  console.log(
+    chalk.yellow(figlet.textSync("My Node CLI", { horizontalLayout: "full" }))
+  );
+
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "choice",
+        message: "Choose an option:",
+        choices: ["Option 1", "Option 2", "Option 3"],
+      },
+    ])
+    .then((result) => {
+      const spinner = ora(`Doing ${result.choice}...`).start(); // Start the spinner
+
+      setTimeout(() => {
+        spinner.succeed(chalk.green("Done!"));
+      }, 3000);
+    });
+});
 
 program.parse(process.argv);
