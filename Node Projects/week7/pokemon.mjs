4c08b248ede99bb885ac9f9308d12e1f0b35f4d8
jzurmueh <jzurmueh@hs-mittweida.de>
2026-02-15 14:59:32 +0100

    Merged updated scripts

--- Node Projects/week7/pokemon.mjs
+++ Node Projects/week7/pokemon.mjs
@@ -20,9 +20,9 @@ async function question1() {
             totalHeight += height;
         }
     }
-    let averageHeight = totalHeight / numPokemonInFirstGeneration;
+    let averageHeight = (totalHeight / numPokemonInFirstGeneration) / 10; // convert from decimeters to meters
     
-    console.log(`Answer: ${averageHeight}`)
+    console.log(`Answer: ${averageHeight}m`)
 
 }
 
@@ -60,26 +60,140 @@ async function question2() {
 async function question3() {
 
     console.log("Question: How many different Pokemon possess the Levitate ability?");
+
+    const url = `${baseURL}/ability/26/`; // index 26 is the levitate ability in the api
+    const response = await fetchAndCache(url);
+
+    const pokemonWithLevitateAbility = response.pokemon; // list of all pokemon with the levitate ability
+
+    const result = pokemonWithLevitateAbility.length;
+
+    console.log(`Answer: ${result}`);
     
+}
+
+
+async function question4() {
+
+    console.log("Question: Which Pokemon has the highest Speed stat among those that weigh less than 10kg?")
+
+    // Prepare variables
+    let highestSpeed = 0;
+    let highestSpeedPokemonName = "";
+    const response = await fetchAndCache(`${baseURL}/pokemon/?limit=2000`) //extend the limit to fetch all Pokemon
+    const allPokemon = response.results;
+
+    for (let i = 0; i < allPokemon.length; i++) {
+        const url = allPokemon[i].url;
+        const pokemon = await fetchAndCache(url);
+        const weight = pokemon.weight;
+        const speed = pokemon.stats[5].base_stat;
+
+        if (weight < 100 && speed > highestSpeed) { // 10kg = 100 hectograms
+            highestSpeed = speed;
+            highestSpeedPokemonName = pokemon.name;
+        }
+    }
     
+    console.log(`Answer: ${highestSpeedPokemonName}`);
+}
+
+
+async function question5() {
+
+    console.log("Question: Which 'Electric' type Pokemon has the shortest height?");
+
+    const url = `${baseURL}/type/13/`; // index 13 is the fire type in the api
+    const response = await fetchAndCache(url);
+
+    const electricPokemon = response.pokemon; // list of all electric type Pokemon
+
+    let shortestHeight = Infinity;
+    let shortestHeightPokemonName = "";
+
+
+    for (let i = 0; i < electricPokemon.length; i++) {
+
+        const url = electricPokemon[i].pokemon.url;
+        const pokemonData = await fetchAndCache(url);
+        
+        if (pokemonData.height < shortestHeight) {
+            shortestHeight = pokemonData.height;
+            shortestHeightPokemonName = pokemonData.name;
+        }
+    }
     
+    console.log(`Answer: ${shortestHeightPokemonName}`);
+
+}
+
+
+async function question6() {
+
+    console.log("Question: What is the heaviest Pokemon?");
+
+    let highestWeight = 0;
+    let highestWeightPokemonName = "";
+
+    const url = `${baseURL}/pokemon/?limit=2000`;
+    const response = await fetchAndCache(url);
+    const allPokemon = response.results;
+
+    for (let i = 0; i < allPokemon.length; i++) {
+        const url = allPokemon[i].url;
+        const pokemonData = await fetchAndCache(url);
+        const weight = pokemonData.weight;
+
+        if (weight > highestWeight) {
+            highestWeight = weight;
+            highestWeightPokemonName = pokemonData.name;
+        }
+    }
+    
+    console.log(`Answer: ${highestWeightPokemonName}`);
+
+}
+
+
+async function question7() {
+
+    console.log("What are the Pokemon with the longest names?");
+
+    let longestName = 0;
+    let longestNamePokemon = [];
+
+    const url = `${baseURL}/pokemon/?limit=2000`;
+    const response = await fetchAndCache(url);
+    const allPokemon = response.results;
+
+    // method: Enter the name of every Pokemon which name is the length of 'longestName'.
+    // Every time we find a longer name, clear the array and update 'longestName'
+    for (let i = 0; i < allPokemon.length; i++) {
+        
+        const url = allPokemon[i].url;
+        const pokemonData = await fetchAndCache(url);
+        const name = pokemonData.name;
+
+        if (name.length > longestName) {
+            longestName = name.length;
+            longestNamePokemon = [];
+        }
+        if (name.length === longestName) {
+            longestNamePokemon.push(name);
+        }
+
+    }
+
+    console.log(`Answer: ${longestNamePokemon}`);
+
 }
 
 
 
 await question1();
 await question2();
-// question3();
-// question4();
-// question5();
-// question6();
-// question7();
-
-
-// * What is the average height of all Pokemon in the first generation?
-// * Which Pokemon has the highest "Base Experience" among all Fire type Pokemon?
-// * How many different Pokemon possess the "Levitate" ability?
-// * Which Pokemon has the highest "Speed" stat among those that weigh less than 10kg?
-// * Which "Electric" type Pokemon has the shortest height?
-// * What is the heaviest Pokemon?
-// * What are the Pokemons with the longest names?
+await question3();
+await question4();
+await question5();
+await question6();
+await question7();
